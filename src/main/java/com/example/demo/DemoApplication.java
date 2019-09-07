cfc581460477181a7c092da3fee48b47c66ac4e2
江尊 <79894179@qq.com>
2019-09-07 09:25:46 +0800

    int b=12

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         int a=12;
+        int b=9;
         System.out.println(123);
         SpringApplication.run(DemoApplication.class, args);
     }
